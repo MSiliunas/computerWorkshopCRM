ccb13d93a6ef7ccf2c68366fcc2f28e51e831f7d
Marijus Siliunas <marijus.siliunas@gmail.com>
2015-12-15 17:33:20 +0200

    rubocop autofix

--- workshop-crm/spec/spec_helper.rb
+++ workshop-crm/spec/spec_helper.rb
@@ -41,10 +41,10 @@ RSpec.configure do |config|
   config.mock_with :mocha
 
   # config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    # mocks.verify_partial_doubles = true
+  # Prevents you from mocking or stubbing a method that does not exist on
+  # a real object. This is generally recommended, and will default to
+  # `true` in RSpec 4.
+  # mocks.verify_partial_doubles = true
   # end
 
   # The settings below are suggested to provide a good initial experience
